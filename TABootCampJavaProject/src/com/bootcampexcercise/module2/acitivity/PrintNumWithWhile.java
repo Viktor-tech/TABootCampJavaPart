540891f30fb593c6ef10dff17c8ad342f1280d2c
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 14:04:43 +0300

    PrintNumWithWhile update

---
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/PrintNumWithWhile.java
@@ -0,0 +1,18 @@
+package com.bootcampexcercise.module2.acitivity;
+
+public class PrintNumWithWhile {
+
+public static void main(String[] args){
+    int i = 1;
+    while(i < 100){
+        System.out.println(i);
+        i=i+2;
+    }
+
+
+
+}
+
+
+
+}
