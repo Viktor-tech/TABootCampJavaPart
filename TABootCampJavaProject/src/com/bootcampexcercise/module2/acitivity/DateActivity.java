31679f5fdabedf04577778a65a5e890e49289727
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 14:19:55 +0300

    date activity

---
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/DateActivity.java
@@ -0,0 +1,14 @@
+package com.bootcampexcercise.module2.acitivity;
+
+import java.util.Date;
+
+
+public class DateActivity {
+    public static void main (String arggs []) {
+        Date dateToday = new Date();
+        System.out.println(dateToday);
+
+    }
+
+
+}
