7511fbbc25a5c12f4e2e04094c2ede1e2eb9a875
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 10:52:18 +0300

    addwhole numbers task, Activity 5

---
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/AddWholeNum.java
@@ -0,0 +1,13 @@
+package com.bootcampexcercise.module2.acitivity;
+
+public class AddWholeNum {
+
+    public static void main(String[] args){
+        int total =0;
+        for (int i = 50; i <= 50; i++) {
+            total = total + i;
+        }
+        System.out.println("Addition of whole numbers from 50 to 100 is "+ total);
+    }
+
+}
