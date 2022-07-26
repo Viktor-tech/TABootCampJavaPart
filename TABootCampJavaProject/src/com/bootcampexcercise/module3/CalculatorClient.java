a09e2617391a5a3be495e6ca0098e85a3385d9cc
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 15:28:15 +0300

    calculator client update

--- TABootCampJavaProject/src/com/bootcampexcercise/module3/CalculatorClient.java
+++ TABootCampJavaProject/src/com/bootcampexcercise/module3/CalculatorClient.java
@@ -1,14 +1,7 @@
 package com.bootcampexcercise.module3;
 
-/**
- * @author
- *
- */
 public class CalculatorClient {
 
-    /**
-     * @param args
-     */
     public static void main(String[] args) {
 
         /*Instantiate Normal Calculator. Note that CALC_TYPE_NORMAL is a public static
@@ -24,7 +17,7 @@ public class CalculatorClient {
 
 
         // Multiply
-        int[] mul = {2, 3, 4};
+        int[] multiply = {2, 3, 4};
         //TODO Complete the code: result should be equal to the normalCalc multiply function and int array mul should be multiplied.
         System.out.println("2) Multiplication result is " + result);
 
@@ -33,13 +26,16 @@ public class CalculatorClient {
         //TODO Write code which will use normalCalc and divide function
         //TODO Divide numbers should be 10 and 2
         //TODO Divide result should be printed in similar style as multiply and addition
-
+        result = normalCalc.divide(10,2);
+        System.out.println("3) Division result is " + result);
 
 
         //Subtraction
         //TODO Write code which will use normalCalc and subtract function
         //TODO Subtract numbers should be 10 and 2
         //TODO Subtract result should be printed in similar style as multiply and addition
+        result = normalCalc.subtract(10,2);
+        System.out.println("4) Subtract result is " + result);
     }
 
 
