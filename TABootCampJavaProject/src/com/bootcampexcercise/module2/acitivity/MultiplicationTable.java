d0ec6352c67be1564bce7875e2cce6fb61c11383
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 11:51:58 +0300

    NumToWordsWithMethodCalling

---
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/MultiplicationTable.java
@@ -0,0 +1,21 @@
+package com.bootcampexcercise.module2.acitivity;
+
+public class MultiplicationTable {
+
+    public static  void main(String[] args){
+
+        for(int i = 11; i <= 20; i++){
+            System.out.println("Table of" +i);
+          for (int j = 11; j <= 20; j++){
+              System.out.println(i + "x" + j + " = " + i*j);
+          }
+        }
+
+
+
+
+
+
+    }
+
+}
