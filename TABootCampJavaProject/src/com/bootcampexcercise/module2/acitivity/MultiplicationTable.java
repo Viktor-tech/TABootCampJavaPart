a0b242fe97d720cc89dc05dae59545673ae65c03
Viktors Licovs <vlitsov@gmail.com>
2022-07-26 15:41:52 +0300

    Activity 6 update

--- TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/MultiplicationTable.java
+++ TABootCampJavaProject/src/com/bootcampexcercise/module2/acitivity/MultiplicationTable.java
@@ -5,9 +5,9 @@ public class MultiplicationTable {
     public static  void main(String[] args){
 
         for(int i = 11; i <= 20; i++){
-            System.out.println("Table of" +i);
-          for (int j = 11; j <= 20; j++){
-              System.out.println(i + "x" + j + " = " + i*j);
+            System.out.println("Table of " +i);
+          for (int j = 1; j <= 20; j++){
+              System.out.println(i + " x " + j + " = " + i*j);
           }
         }
 
